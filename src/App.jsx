2ab08d0ea913bb2b5aed1b707f431134cfd4b78c
Daya Chettri <Dayasagar073@gmail.com>
2023-03-30 22:44:17 +0530

    ✅

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import Table from './components/Table';
+
 function App() {
-  return <div>App</div>;
+  const data = [
+    { name: 'Daya', city: 'Manali', age: 21, id: 1 },
+    { name: 'Ashu', city: 'Lucknow', age: 25, id: 2 },
+    { name: 'Ravi', city: 'Delhi', age: 28, id: 3 },
+    { name: 'Abhilash', city: 'Patna', age: 25, id: 4 },
+  ];
+
+  const config = [{ label: 'Name' }, { label: 'City' }, { label: 'Age' }];
+
+  return (
+    <main>
+      <Table data={data} config={config} />
+    </main>
+  );
 }
 
 export default App;
